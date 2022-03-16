82b04d51f6698d9070f4ecf8434b859e17dd2626
leeseungmin0 <tmdalsdl112@naver.com>
2022-03-16 16:13:00 +0900

    js .area1 code change

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const homeText_1 = document.querySelector(".area1,.area2")
+const homeText_1 = document.querySelector(".area1")
 window.addEventListener('scroll',function(){
     const value = window.scrollY
-    if(value>200){
+    if(value>250){
         homeText_1.style.animation = "back__slide 1.5s ease-out forwards"
     }else{
         homeText_1.style.animation = "slide 1.5s ease-out"
@@ -10,8 +10,8 @@ window.addEventListener('scroll',function(){
 
 const homeText_2 = document.querySelector(".area2")
 window.addEventListener('scroll',function(){
-    const value = window.scrollY
-    if(value>750){
+    const value2 = window.scrollY
+    if(value2>700){
         homeText_2.style.animation = "slide 1.5s ease-out"
     }else{
         homeText_2.style.animation = "back__slide 1.5s ease-out forwards"
@@ -20,8 +20,8 @@ window.addEventListener('scroll',function(){
 
 const homeText_3 = document.querySelector(".area3")
 window.addEventListener('scroll',function(){
-    const value = window.scrollY
-    if(value>1500){
+    const value3 = window.scrollY
+    if(value3>1500){
         homeText_3.style.animation = "slide 1.5s ease-out"
     }else{
         homeText_3.style.animation = "back__slide 1.5s ease-out forwards"
@@ -30,8 +30,8 @@ window.addEventListener('scroll',function(){
 
 const homeText_4 = document.querySelector(".area4")
 window.addEventListener('scroll',function(){
-    const value = window.scrollY
-    if(value>2250){
+    const value4 = window.scrollY
+    if(value4>2250){
         homeText_4.style.animation = "slide 1.5s ease-out"
     }else{
         homeText_4.style.animation = "back__slide 1.5s ease-out forwards"
